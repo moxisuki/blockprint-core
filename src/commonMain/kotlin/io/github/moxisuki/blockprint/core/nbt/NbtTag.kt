d03ad753000c8ca1b7886a2ae665c610c57e37b2
Moix <59305689+moxisuki@users.noreply.github.com>
2026-07-02 06:09:48 +0800

    refactor(nbt): add nbt/ typealias shells

---
+++ src/commonMain/kotlin/io/github/moxisuki/blockprint/core/nbt/NbtTag.kt
@@ -0,0 +1,7 @@
+@file:Suppress("PackageDirectoryMismatch")
+
+package io.github.moxisuki.blockprint.core.nbt
+
+import io.github.moxisuki.blockprint.core.NbtTag
+
+typealias NbtTag = io.github.moxisuki.blockprint.core.NbtTag
---
+++ src/commonMain/kotlin/io/github/moxisuki/blockprint/core/nbt/NbtWriter.kt
@@ -0,0 +1,7 @@
+@file:Suppress("PackageDirectoryMismatch")
+
+package io.github.moxisuki.blockprint.core.nbt
+
+import io.github.moxisuki.blockprint.core.NbtWriter
+
+typealias NbtWriter = io.github.moxisuki.blockprint.core.NbtWriter
