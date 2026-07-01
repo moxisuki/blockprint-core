05a7388d94e89b6e986c2d0db4ddd2b150fa642c
Moix <59305689+moxisuki@users.noreply.github.com>
2026-07-02 06:13:58 +0800

    feat(internal): extract shared NBT accessors

---
+++ src/commonMain/kotlin/io/github/moxisuki/blockprint/core/exceptions/BlockPrintException.kt
@@ -0,0 +1,11 @@
+@file:Suppress("PackageDirectoryMismatch")
+
+package io.github.moxisuki.blockprint.core.exceptions
+
+import io.github.moxisuki.blockprint.core.exceptions.LitematicException
+
+/**
+ * Temporary typealias used during migration to the BlockPrintReader rewrite.
+ * Task 3.2 will replace this with the real `BlockPrintException` class.
+ */
+typealias BlockPrintException = LitematicException
